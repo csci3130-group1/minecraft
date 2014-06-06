c42a96529885190fd19cb721f07a20361a5c1b88
Andrey Kulakevich <andrey@cs.dal.ca>
2014-06-06 14:33:42 -0300

    Initial Simple Natural Resource Block

---
+++ forge/src/main/java/com/example/examplemod/NaturalResourceBlock.java
@@ -0,0 +1,12 @@
+package com.example.examplemod;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class NaturalResourceBlock extends Block {
+
+	protected NaturalResourceBlock(Material m) {
+		super(m);
+	}
+	
+}
