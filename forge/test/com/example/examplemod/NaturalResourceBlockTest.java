9ba348ff43b91bb964e974e3eb6f3a9203436de3
Andrey Kulakevich <andrey@cs.dal.ca>
2014-06-09 16:08:43 -0300

    Added initial test for the natural resource block.

---
+++ forge/test/com/example/examplemod/NaturalResourceBlockTest.java
@@ -0,0 +1,14 @@
+package com.example.examplemod;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NaturalResourceBlockTest {
+
+	@Test
+	public void testBlock() {
+		fail("Not idea how to test a block :D");
+	}
+
+}
