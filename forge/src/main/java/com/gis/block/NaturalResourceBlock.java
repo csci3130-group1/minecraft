6c2c6e2844b5581d3d26ce8ca9db0d36ab3de9a2
aralko <adam@ralko.com>
2014-06-10 17:24:55 -0300

    Modified folder structure to put block classes in own folder.

---
+++ forge/src/main/java/com/gis/block/NaturalResourceBlock.java
@@ -0,0 +1,12 @@
+package com.gis.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class NaturalResourceBlock extends Block {
+
+	public NaturalResourceBlock(Material m) {
+		super(m);
+	}
+
+}
