c542967c9935f38c070355c92d6f44a0feca6850
aralko <adam@ralko.com>
2014-06-10 17:22:58 -0300

    Added EntityWorker class files.

--- forge/src/main/java/com/gis/entity/AbstractEntity.java
+++ forge/src/main/java/com/gis/entity/AbstractEntity.java
@@ -1,5 +1,42 @@
 package com.gis.entity;
 
-public abstract class AbstractEntity {
+import java.util.Map;
 
+import com.gis.core.Constants;
+import com.gis.core.GIS;
+
+import net.minecraft.world.World;
+
+public abstract class AbstractEntity extends AbstractSerializableEntity {
+
+	public int gisID;
+	
+	public AbstractEntity(World world)
+	{
+		super(world);
+
+		//Get the appropriate GIS id for the person.
+		if (!world.isRemote)
+		{
+			for (final Map.Entry<Integer, Integer> mapEntry : GIS.getInstance().idsMap.entrySet())
+			{
+				if (mapEntry.getKey() > gisID)
+				{
+					gisID = mapEntry.getKey();
+				}
+			}
+
+			gisID++;
+
+			//Put the ID in the list.
+			GIS.getInstance().idsMap.put(gisID, getEntityId());
+		}
+
+		//getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(MCA.getInstance().getModProperties().villagerBaseHealth);
+		//setHealth(GIS.getInstance().getModProperties().villagerBaseHealth);
+		setHealth(20);
+		setSize(Constants.WIDTH_ADULT, Constants.HEIGHT_ADULT);
+	}
+
+	
 }
--- forge/src/main/java/com/gis/entity/EntityWorker.java
+++ forge/src/main/java/com/gis/entity/EntityWorker.java
@@ -1,5 +1,24 @@
 package com.gis.entity;
 
-public class EntityWorker extends AbstractEntity {
+import net.minecraft.entity.EntityAgeable;
+import net.minecraft.entity.EnumCreatureAttribute;
+import net.minecraft.entity.passive.EntityAmbientCreature;
+import net.minecraft.entity.passive.EntityCow;
+import net.minecraft.world.World;
+ 
+public class EntityWorker extends EntityCow
+{
+    public EntityWorker(World world)
+    {
+        super(world);
+    }
+ 
+    public EnumCreatureAttribute getCreatureAttribute()
+    {
+        return EnumCreatureAttribute.UNDEAD;
+    }
 
+	public EntityCow createChild(EntityAgeable var1) {
+		return null;
+	}
 }
