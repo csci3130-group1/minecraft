42bd63405f6bf597f02a778ac2574b0a134d0464
aralko <adam@ralko.com>
2014-06-10 17:21:40 -0300

    Added WorkerEntity spawn. Refactored project folder structure.

--- forge/src/main/java/com/gis/core/Constants.java
+++ forge/src/main/java/com/gis/core/Constants.java
@@ -1,5 +1,8 @@
 package com.gis.core;
 
 public class Constants {
+	
+	public static final float HEIGHT_ADULT = 1.8F;
+	public static final float WIDTH_ADULT = 0.6F;
 
 }
--- forge/src/main/java/com/gis/core/GIS.java
+++ forge/src/main/java/com/gis/core/GIS.java
@@ -1,26 +1,46 @@
 package com.gis.core;
 
+import java.util.HashMap;
+import java.util.Map;
 
+import com.gis.block.*;
+import com.gis.entity.*;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EnumCreatureType;
 import net.minecraft.init.Blocks;
+import net.minecraft.world.biome.BiomeGenBase;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.registry.EntityRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.common.registry.LanguageRegistry;
 
 @Mod(modid = GIS.MODID, version = GIS.VERSION)
 public class GIS
 {
-    public static final String MODID = "examplemod";
+	/** An instance of the core GIS class. */
+	@Instance("GIS")
+	private static GIS instance;
+		
+    public static final String MODID = "gis";
     public static final String VERSION = "1.0";
     
+    /**Map of GIS ids and entity ids. Key = mcaId, Value = entityId.**/
+	public Map<Integer, Integer> idsMap = new HashMap<Integer, Integer>();
+
+	/**Map of GIS ids and their associated entity. Key = mcaId, Value = abstractEntity. */
+	public Map<Integer, AbstractEntity> entitiesMap = new HashMap<Integer, AbstractEntity>();
+    
+
+	
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-		// some example code
         System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
     }
     @EventHandler
@@ -32,5 +52,35 @@ public class GIS
     		.setBlockName("modBlock")
     		.setCreativeTab(CreativeTabs.tabBlock);
     	GameRegistry.registerBlock(modBlock, "modBlock");
+    	
+    	Block naturalResource = new NaturalResourceBlock(Material.iron)
+			.setBlockName("Natural Resource")
+			.setHardness(0.8f)
+			.setStepSound(Block.soundTypeStone);
+    	GameRegistry.registerBlock(naturalResource, "Natural Resource");
+    	
+    	//Insantiate worker entities
+    	EntityRegistry.registerGlobalEntityID(EntityWorker.class, "Worker", EntityRegistry.findGlobalUniqueEntityId(), 1, 2);
+    	
+		EntityRegistry.registerModEntity(EntityWorker.class, "Worker", 1, this, 50, 2, true);
+		EntityRegistry.addSpawn(EntityWorker.class, 1, 10, 10, EnumCreatureType.creature);
+
+
     }
+
+    
+    /**
+	 * @return	An instance of GIS.
+	 */
+	public static GIS getInstance()
+	{
+		return instance;
+	}
+	
+	/*
+	public ModPropertiesList getModProperties()
+	{
+		//return (ModPropertiesList)modPropertiesManager.modPropertiesInstance;
+	}
+	*/
 }
