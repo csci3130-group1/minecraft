9b015aee92c7e15e3914180f58a8842949d09e15
Andrey Kulakevich <andrey@cs.dal.ca>
2014-06-10 18:03:47 -0300

    removed code that did not allow compile, had conflict with other files

--- forge/src/main/java/com/example/examplemod/ResourceMountain.java
+++ forge/src/main/java/com/example/examplemod/ResourceMountain.java
@@ -10,19 +10,8 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ResourceMountain implements IWorldGenerator
 {
-    public static final String MODID = "nrmountain";
-    public static final String VERSION = "1.0";
-    
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-		// some example code
-        System.out.println("creating natural resource mountians!");
-    }
-
 	@Override
 	public void generate(Random random, int chunkX, int chunkZ, World world,
 			IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
