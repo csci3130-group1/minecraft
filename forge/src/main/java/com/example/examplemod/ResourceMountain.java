feb1108405c9e587c11e5ad6625c5bd4cbe6aec2
viviankou <expandwings@live.cn>
2014-06-10 17:25:00 -0300

    update resource mountain

--- forge/src/main/java/com/example/examplemod/ResourceMountain.java
+++ forge/src/main/java/com/example/examplemod/ResourceMountain.java
@@ -1,6 +1,11 @@
 package com.example.examplemod;
 
+import java.util.Random;
+
 import net.minecraft.init.Blocks;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.IChunkProvider;
+import cpw.mods.fml.common.IWorldGenerator;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
@@ -17,10 +22,12 @@ public class ResourceMountain implements IWorldGenerator
 		// some example code
         System.out.println("creating natural resource mountians!");
     }
-    
-    @Override
-    public void generate(Random random, int chunkX, int chunkZ, World world) {
-        world.setBlock(chunkX*16 + random.nextInt(16), 100, chunkZ*16 + random.nextInt(16), 5);
-        // block X location, block Y location, block Z location, block id (in this case, wooden planks)
-    }
+
+	@Override
+	public void generate(Random random, int chunkX, int chunkZ, World world,
+			IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
+		// TODO Auto-generated method stub
+		world.setBlock(chunkX*16 + random.nextInt(16), 100, chunkZ*16 + random.nextInt(16), null, 5, chunkZ);
+		
+	}
 }
