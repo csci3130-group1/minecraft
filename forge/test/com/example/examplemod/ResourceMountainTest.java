0da7f618835aa7110faa1abe3753563cd09f5cd5
viviankou <expandwings@live.cn>
2014-06-12 18:11:32 -0300

    Add Tests For Resource Mountain

---
+++ forge/test/com/example/examplemod/ResourceMountainTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+
+
+public class ResourceMountainTest {
+
+	@Test
+	public void testWorldGen() {
+		assertEqual("world", null);
+	}
+	@Test
+	public void testMountGen(){
+		assertEqual("mount", null);
+	}
+}
