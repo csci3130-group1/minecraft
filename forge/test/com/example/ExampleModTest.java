c1a181000045951104a8393c39661baf3d4298d7
viviankou <expandwings@live.cn>
2014-07-22 23:02:16 -0300

    Add example test, logichelper test and point3d test

---
+++ forge/test/com/example/ExampleModTest.java
@@ -0,0 +1,23 @@
+package com.example.examplemod;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ExampleModTest {
+	ExampleMod ex;
+
+	@Before
+	public void ExampleModInit() {
+		ex = new ExampleMod();
+	}
+
+	
+	@Test
+	public void Test() {
+		Assert.assertTrue(true);
+	}
+
+}
